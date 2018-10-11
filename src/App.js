1da62623bc844540eb0943a44c4c63e35e5bcba6
Kunigaz <kunigaz7@gmail.com>
2018-10-11 07:21:44 -0500

    created debt entry form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { FormControl, InputLabel, Input } from "@material-ui/core";
+import "./App.css";
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      debts: [],
+      debtName: "",
+      debtApr: 0.0,
+      amount: "",
+    };
+  }
+
+  handleFormChange = prop => event => {
+    this.setState({ [prop]: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GlobalHeader />
+        <DebtEntryForm handleFormChange={this.handleFormChange} />
+        {this.props.children}
       </div>
     );
   }
 }
 
-export default App;
+const GlobalHeader = () => {
+  return <header className="App-header"> React Debt Calculator </header>;
+};
+
+const DebtEntryForm = ({ debtName, handleFormChange }) => {
+  return (
+    <div className="App-form">
+      <FormControl>
+        <InputLabel htmlFor="debt-name">Name</InputLabel>
+        <Input id="debt-name" onChange={handleFormChange("debtName")} />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="debt-apr">APR</InputLabel>
+        <Input id="debt-apr" onChange={handleFormChange("debtApr")} />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="debt-apr">Amount</InputLabel>
+        <Input id="debt-apr" onChange={handleFormChange("amount")} />
+      </FormControl>
+    </div>
+  );
+};
